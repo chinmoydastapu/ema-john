5d87b988365988e1fa895c7b564b1aebb4e6b04d
Chinmoy Das <chinmoydastapu2@gmail.com>
2023-05-03 00:53:13 +0600

    Order component redesigned for providing extra information

--- src/components/MyOrders/Order.jsx
+++ src/components/MyOrders/Order.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import { MinusCircleIcon, PlusCircleIcon, TrashIcon } from "@heroicons/react/24/solid";
+import { MinusCircleIcon, PlusCircleIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useContext, useState } from "react";
 import { ThemeContext } from "../../App";
 import { setQuantityToLS } from "../../utilities/LocalStorage";
@@ -11,20 +11,23 @@ const Order = ({ orderedProduct, handleTrashBtn }) => {
 
     const [toggleTheme] = useContext(ThemeContext);
 
-    const { id, name, price, shipping, img } = orderedProduct;
-    const nameParts = name.split(' ').slice(0, 3);
-    const updatedName = nameParts.join(' ');
+    const { id, name, price, shipping, img, stock } = orderedProduct;
 
     const handleIncreaseQuantity = () => {
         const quantity = parseInt(orderQuantity) + 1;
-        setOrderQuantity(quantity);
-        orderedProduct.quantity = quantity;
+        if (quantity <= stock) {
+            setOrderQuantity(quantity);
+            orderedProduct.quantity = quantity;
+        } else {
+            setOrderQuantity(stock);
+            orderedProduct.quantity = stock;
+        }
         setQuantityToLS(id, orderedProduct.quantity);
     };
 
     const handleDecreaseQuantity = () => {
         const quantity = parseInt(orderQuantity) - 1;
-        if(quantity > 1) {
+        if (quantity > 1) {
             setOrderQuantity(quantity);
             orderedProduct.quantity = orderQuantity;
         } else {
@@ -36,9 +39,13 @@ const Order = ({ orderedProduct, handleTrashBtn }) => {
 
     const handleQuantityChange = event => {
         const quantity = !isNaN(event.target.value) ? event.target.value : 1;
-        setOrderQuantity(quantity);
-
-        orderedProduct.quantity = isNaN(parseInt(quantity)) ? 1 : parseInt(quantity);
+        if (quantity <= stock) {
+            setOrderQuantity(quantity);
+            orderedProduct.quantity = isNaN(parseInt(quantity)) ? 1 : parseInt(quantity);
+        } else {
+            setOrderQuantity(stock);
+            orderedProduct.quantity = stock;
+        }
         setQuantityToLS(id, orderedProduct.quantity);
     };
 
@@ -47,29 +54,31 @@ const Order = ({ orderedProduct, handleTrashBtn }) => {
     }, 10);
 
     return (
-        <div className={`flex justify-between items-center m-5 my-10 p-2 sm:p-3 rounded-lg shadow-lg hover:shadow-xl transition-all duration-1000 ease-in-out ${toggleTheme ? 'bg-slate-200' : 'bg-slate-800'} ${initialShow ? 'opacity-100' : 'scale-50 opacity-0 -translate-x-96'}`}>
+        <div className={`relative flex justify-between items-center m-5 my-10 p-2 sm:p-3 rounded-lg shadow-lg hover:shadow-xl transition-all duration-1000 ease-in-out ${toggleTheme ? 'bg-slate-200' : 'bg-slate-800'} ${initialShow ? 'opacity-100' : 'scale-50 opacity-0 -translate-x-96'}`}>
             <div className="flex justify-start items-center gap-5">
-                <div>
-                    <img className="w-20 sm:w-24 h-24 rounded-lg" src={img} alt="Loading..." />
+                <div className="w-20 sm:w-24 h-24">
+                    <img className="rounded-lg h-full" src={img} alt="Loading..." />
                 </div>
                 <div className="font-semibold">
-                    <h3 className="text-left text-sm sm:text-xl font-semibold">{updatedName}</h3>
-                    <div className="text-left text-sm">
-                        <p>Price: <span className="text-orange-400">${price}</span></p>
-                        <p>Shipping: <span className="text-orange-400">${shipping}</span></p>
-                    </div>
-                    <div className="flex justify-start items-center gap-2 mt-3">
-                        <label htmlFor="order-quantity" className="text-sm">Quantity: </label>
-                        <MinusCircleIcon className="w-6 h-6 text-orange-400 cursor-pointer" onClick={handleDecreaseQuantity} />
-                        <form>
-                            <input id="order-quantity" type="text" className="py-[3px] w-10 focus:outline-none text-center text-sm  bg-orange-300 text-black rounded-md" value={orderQuantity}  onChange={handleQuantityChange} />
-                        </form>
-                        <PlusCircleIcon className="w-6 h-6 text-orange-400 cursor-pointer" onClick={handleIncreaseQuantity} />
+                    <h3 className="text-left text-sm sm:text-xl font-semibold">{name}</h3>
+                    <div className="flex justify-start items-center gap-16 sm:gap-28">
+                        <div className="text-left text-sm">
+                            <p>Price: <span className="text-orange-400">${price}</span></p>
+                            <p>Shipping: <span className="text-orange-400">${shipping}</span></p>
+                            <p>Stock: <span className="text-orange-400">{stock}</span></p>
+                        </div>
+                        <div className="flex justify-start items-center gap-2">
+                            <MinusCircleIcon className="w-6 h-6 text-orange-400 cursor-pointer" onClick={handleDecreaseQuantity} />
+                            <form>
+                                <input type="text" className="py-[3px] w-10 focus:outline-none text-center text-sm  bg-orange-300 text-black rounded-md" value={orderQuantity} onChange={handleQuantityChange} />
+                            </form>
+                            <PlusCircleIcon className="w-6 h-6 text-orange-400 cursor-pointer" onClick={handleIncreaseQuantity} />
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="flex justify-between items-center gap-4 cursor-pointer" onClick={() => handleTrashBtn(id)}>
-                <TrashIcon className="w-10 h-10 bg-orange-300 text-orange-600 p-2 rounded-full" />
+            <div className="absolute -top-3 -right-3 cursor-pointer" onClick={() => handleTrashBtn(id)}>
+                <XMarkIcon className="w-8 h-8 bg-orange-300 text-orange-600 p-2 rounded-full" />
             </div>
         </div>
     );
